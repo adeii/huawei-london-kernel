5747599af99d72e916a2c96fe8e2db9a48015818
adeii <adeii@dr.com>
2020-08-27 00:40:50 +0200

    Goes to 3.18.95

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3647,9 +3647,7 @@ static void ffs_closed(struct ffs_data *ffs)
 	ci = opts->func_inst.group.cg_item.ci_parent->ci_parent;
 	ffs_dev_unlock();
 
-	if (test_bit(FFS_FL_BOUND, &ffs->flags))
-		unregister_gadget_item(ci);
-	return
+	return;
 done:
 	ffs_dev_unlock();
 }
